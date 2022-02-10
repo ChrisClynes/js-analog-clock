de509a2d8cbf3614d2fe46a1424bb603d03f7703
Chris Clynes <chrisclynes@live.com>
2022-02-09 20:34:56 -0500

    fixed bug with display in Firefox and Safari

--- index.js
+++ index.js
@@ -8,39 +8,38 @@ positionNumbers=() => {
             createNumberDiv.classList.add('clock-numbers');
             createNumberDiv.innerText= i;
             clockNumbers.appendChild(createNumberDiv);
-            console.log("clock " + i);
+            //loops through 1-12 to create divs and set id and class, may remove to speed up onload performance
     }
 }
 
 scaleFontSize=() => {
     let clockWidth = document.getElementById("clock-container").clientWidth;
     let numbers = document.getElementsByClassName("clock-numbers");
-    let signature = document.getElementById("signature")
+    let signature = document.getElementById("signature");
     let font = Math.floor(clockWidth / 15);
     for (let i = 0; i < numbers.length; i++) {
         let hour = numbers[i];
-        hour.style.fontSize = font + "px";
+        hour.style.fontSize = font + "px";// loop through all clock numbers and set font size based off clock width
       }
-    signature.style.fontSize = font/1.5 + "px";
+    signature.style.fontSize = font/1.5 + "px";// use font ration from above for signature text too 
 }
 
 function calcTime() {
     let time = new Date();
-    let hours = (time.toLocaleString(navigator.language, {hour: 'numeric'}).match(/\d+/)[0]);//extracts hour in 12hr format
+    let hours = (time.toLocaleString('en-US', {hour: 'numeric'}).match(/\d+/)[0]);//extracts hour in 12hr format
     let secondsDeg = time.getSeconds() * 6;//converts seconds to degrees
     let minutesDeg = ((time.getMinutes() * 6) + (secondsDeg / 60));//minutes * 6 gives base degree then add secondsDeg / 60 
-    let hoursDeg = ((hours * 30) + (minutesDeg / 12));
+    let hoursDeg = ((hours * 30) + (minutesDeg / 12));// hours * 30 degrees plus minutes /12 to get 1 hour ratio
     const secondHand = document.getElementById("second-hand");
     const minuteHand = document.getElementById("minute-hand");
     const hourHand = document.getElementById("hour-hand");
-    secondHand.style.transform = "rotate(" + secondsDeg + "deg)"; 
-    minuteHand.style.transform = "rotate(" + minutesDeg + "deg)";
-    hourHand.style.transform = "rotate(" + hoursDeg + "deg)";
-    console.log(hoursDeg)
-}
+        secondHand.style.transform = "rotate(" + secondsDeg + "deg)"; //sets rotation angle for clock hands
+        minuteHand.style.transform = "rotate(" + minutesDeg + "deg)";
+        hourHand.style.transform = "rotate(" + hoursDeg + "deg)";
+    }
 
 window.onload = positionNumbers();
 window.onload = scaleFontSize();
-window.onload = calcTime()
+window.onload = calcTime();
 window.onresize = scaleFontSize;
 
